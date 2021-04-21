bbd53374d16eeb817b7a858f8f0f09ea723a5c89
Higorbraga01 <higorbraga00@gmail.com>
2021-04-21 11:59:42 -0300

    commit initial

---
+++ fundamentos/object.js
@@ -0,0 +1,20 @@
+const prod1 = {};
+prod1.nome = 'Celular Ultra Mega';
+prod1.preco = 4998.90;
+
+console.log(prod1);
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    obj: {
+        nome: 'Bermuda',
+        preco: 80.0
+        ,obj:{
+            nome: 'Camiseta',
+            preco: 65.00
+        }
+    }
+}
+
+console.log(prod2);
